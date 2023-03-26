bc8e4f1130f615874e25160f7b743b294eb121ca
Documenter.jl <documenter@juliadocs.github.io>
2023-03-26 22:09:19 +0000

    build based on 5ea8b0e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = fluxome","category":"page"},{"location":"#fluxome","page":"Home","title":"fluxome","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for fluxome.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [fluxome]","category":"page"}]
+}
