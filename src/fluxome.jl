e9ad2bc4ae9bea9d28804556d556ab446968717c
Cameron Smith <cameron.ray.smith@gmail.com>
2024-04-30 13:12:11 -0400

    fix(fluxome): use plot_and_save_distribution_timepoints

--- src/fluxome.jl
+++ src/fluxome.jl
@@ -3,9 +3,10 @@ module fluxome
 # base type and utility methods
 include("reactionnetwork.jl")
 export ReactionNetworkSimulation,
-    @reaction_network,
-    setup_reaction_network_reports,
-    save_reaction_network_graph,
-    simulate_reaction_network,
-    plot_and_save_distribution_timepoint
+       @reaction_network,
+       setup_reaction_network_reports,
+       save_reaction_network_graph,
+       simulate_reaction_network,
+       plot_and_save_distribution_timepoint,
+       plot_and_save_distribution_timepoints
 end
